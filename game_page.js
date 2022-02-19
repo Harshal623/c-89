e48e977ddf751f45ea4854b07479766950bab043
Harshal623 <89270049+Harshal623@users.noreply.github.com>
2022-02-19 09:32:55 -0500

    Add files via upload

---
+++ game_page.js
@@ -0,0 +1,10 @@
+player1_name=localStorage.getItem("player1_key")
+player2_name=localStorage.getItem("player2_key")
+player1_score=0
+player2_score=0
+document.getElementById("player1_name").innerHTML = player1_name+" : "
+document.getElementById("player2_name").innerHTML = player2_name+" : "
+document.getElementById("player1_score").innerHTML = player1_score
+document.getElementById("player2_score").innerHTML = player2_score
+document.getElementById("questionturn").innerHTML = "questionturn : "+player1_name
+document.getElementById("answerturn").innerHTML = "answerturn : "+player2_name
